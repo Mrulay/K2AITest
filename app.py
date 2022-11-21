6ee58502e801692a40207b338b798ff2cfd1ad09
Mrulay Mistry <52288782+Mrulay@users.noreply.github.com>
2022-11-21 02:49:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import flask
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from flask import request
 
 app = flask.Flask(__name__)
+
+wordVectors = KeyedVectors.load('w2v.wordvectors')
 
 @app.route('/', methods=['GET'])
 def home():
     arg = request.args['arg1']
     try: 
-        return arg
+        preds = wordVectors.most_similar(arg)
+        preds = [item[0] for item in preds]
+        res = []
+        for num, i in enumerate(preds):
+            if i[2] == 'o':
+                res.append(i)
+        return res
     except KeyError:
         return 'Input does not exist in the data' 
