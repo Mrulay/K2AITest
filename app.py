9e59fcd71cba45420c03c937123429272f4bdeec
Mrulay Mistry <52288782+Mrulay@users.noreply.github.com>
2023-01-07 18:15:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,18 +5,26 @@ from flask import request
 
 app = flask.Flask(__name__)
 
-wordVectors = KeyedVectors.load('w2v.wordvectors')
+wordVectors = KeyedVectors.load(r'C:\Users\Mrulay\Desktop\uWindsor\IRCC Project\Code\w2v.wordvectors')
 
 @app.route('/', methods=['GET'])
 def home():
-    arg = request.args['arg1']
+    content = request.get_json()
     try: 
-        preds = wordVectors.most_similar(arg)
+        #prediction will happen based on the 'responseID'
+        preds = wordVectors.most_similar(content['responseID'], [], 5341)
         preds = [item[0] for item in preds]
         res = []
         for num, i in enumerate(preds):
             if i[2] == 'o':
                 res.append(i)
-        return res
+        if len(res)==0:
+            content['ActionID'] = 'xx'
+            return content
+            #content['Action'] = 'AI Failed to predict'
+        else:
+            content['ActionID'] = res[0]
+            return content
     except KeyError:
         return 'Input does not exist in the data' 
+app.run(host='0.0.0.0', port= 8090)
