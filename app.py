1cd28a4b391cfbb22569fe2fc5c6941a3ab3f881
Mrulay Mistry <52288782+Mrulay@users.noreply.github.com>
2023-01-07 19:50:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ wordVectors = KeyedVectors.load(r'w2v.wordvectors')
 
 @app.route('/', methods=['POST'])
 def home():
-    content = request.get_json()
-    print(content)
+    content = request.data
+    #content = str(content)
+    content = content.decode('UTF-8')
+    content = content[20:-10].rstrip()
+    content = re.sub(r'\r\n', '', content)
+    content = content.replace("\'", "\"")
+    #print(content)
+    content = json.loads(content)
+    #print(type(content["responseId"]))
     try: 
         #prediction will happen based on the 'responseID'
         preds = wordVectors.most_similar(content['responseId'], [], 5341)
@@ -20,11 +27,10 @@ def home():
             if i[2] == 'o':
                 res.append(i)
         if len(res)==0:
-            content['ActionID'] = 'xx'
+            content['ActionId'] = 'xx'
             return content
-            #content['Action'] = 'AI Failed to predict'
         else:
-            content['ActionID'] = res[0]
-            return content
+            content['ActionId'] = res[0]
+        return content
     except KeyError:
-        return 'Input does not exist in the data' 
+        return 'Input does not exist in the data'
